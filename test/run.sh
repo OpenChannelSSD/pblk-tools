407dfa6ae1162cbdb521419b2495c15d7f8f65ee
Simon A. F. Lund <slund@cnexlabs.com>
2017-03-30 17:56:08 +0200

    run

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+if [ $UID -ne 0 ] ; then
+	echo "# FAILED: Must run as root / sudo"
+	exit 1
+fi
 
 DEV_NAME="nvme0n1"
 DEV_PATH="/dev/$DEV_NAME"
 
 PBLK_NAME="pblk0"
 PBLK_PATH="/dev/$PBLK_NAME"
+PBLK_PART_PATH="${PBLK_PATH}p1"
 
 FS_NAME="pblk0fs"
 FS_PATH="/tmp/$FS_NAME"
@@ -28,27 +33,49 @@ NLUNS=$(nvm_dev geo $DEV_PATH | grep "nluns" | cut -d ":" -f 2)
 TLUNS=$(( $NCHANNELS * $NLUNS ))
 ELUN=$(( $TLUNS - 1 ))
 
-# Create pblk instance, filesystem and fill some data into it
 if [ ! -e $PBLK_PATH ]; then
 	echo "# Creating pblk instance"
-	sudo nvme lnvm create -d $DEV_NAME -n $PBLK_NAME -t pblk -b 0 -e $ELUN
+
+	NVME_OUT=$(nvme lnvm create -d $DEV_NAME -n $PBLK_NAME -t pblk -b 0 -e $ELUN)
 	if [ "$?" -ne 0 ]; then
 		echo "# FAILED: exiting"
 		exit 1
 	fi
 fi
 
-if [ ! -e $FS_PATH ]; then
-	mkdir $FS_PATH
-	sudo mkfs -t ext4 $PBLK_PATH
+if [ ! -e $PBLK_PART_PATH ]; then
+	echo "# Creating partition table"
+FDISK_OUT=$(echo "n
+p
+1
+
+
+w
+" | fdisk $PBLK_PATH)
 	if [ "$?" -ne 0 ]; then
-		echo "# FAILED: exiting"
+		echo "# FAILED: creating partition table"
+		exit 1
+	fi
+fi
+
+if [ ! -e $FS_PATH ]; then
+	echo "# Creating mountpoint, filesystem, and mounting"
+
+	MKDIR_OUT=$(mkdir $FS_PATH)
+	if [ "$?" -ne 0 ]; then
+		echo "# FAILED: mkdir"
 		exit 1
 	fi
 
-	sudo mount $PBLK_PATH $FS_PATH
+	MKFS_OUT=$(mkfs -t ext4 $PBLK_PART_PATH)
 	if [ "$?" -ne 0 ]; then
-		echo "# FAILED: exiting"
+		echo "# FAILED: creating file system"
+		exit 1
+	fi
+
+	MOUNT_OUT=$(mount $PBLK_PART_PATH $FS_PATH)
+	if [ "$?" -ne 0 ]; then
+		echo "# FAILED: mounting file system"
 		exit 1
 	fi
 fi
@@ -58,10 +85,14 @@ do
 	FILE_PATH="$FS_PATH/$FILE.rnd"
 	if [ ! -e $FILE_PATH ] || [ "$FILES_OVERWRITE" -eq 1 ]; then
 		echo "# Writing $FILE_PATH"
-		sudo dd if=/dev/urandom of=$FILE_PATH bs=1M count=$FILE
+		DD_OUT=$(dd if=/dev/urandom of=$FILE_PATH bs=1M count=$FILE)
+		if [ "$?" -ne 0 ]; then
+			echo "# FAILED: dd .. $FILE_PATH"
+		fi
 	fi
 done
 
 sync
 
+echo "# Reading meta to $MD_PATH"
 nvm_pblk mdck $DEV_PATH > $MD_PATH
